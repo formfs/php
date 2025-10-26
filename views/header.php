da4a2003920bc3d5de526174cd564b8a1a5824be
duser <user@email.fr>
2025-10-26 14:00:42 +0100

    Ajout cheminb pour la feuille de style

--- views/header.php
+++ views/header.php
@@ -1,3 +1,6 @@
+<?php
+define ("PATH", "http://" . $_SERVER['HTTP_HOST']);
+?>
 <!DOCTYPE html>
 <html lang="en">
 
@@ -5,7 +8,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Posts View</title>
-    <link rel="stylesheet" href="../assets/css/style.css">
+    <link rel="stylesheet" href="<?=PATH;?>/php/assets/css/style.css">
 </head>
 
 <body>
@@ -16,7 +19,7 @@
     <ul>
         <li><a href="">link 1</a></li>
         <li><a href="">link 2</a></li>
-        <li><a href="../posts/contact.php">Contact</a></li>
+        <li><a href="<?=PATH?>/php/posts/contact.php">Contact</a></li>
     </ul>
    </nav>
 </header>
